fd4b854af6a9834aeb99748521a03165f6c172c3
glymphie <ps@glymphie.xyz>
2023-06-09 14:23:25 +0200

    Added basic markers and paths

--- resources/js/redteammap.js
+++ resources/js/redteammap.js
@@ -19,3 +19,47 @@ var bounds = [[0,0],[400,700]];
 var image = './maps/Myst_Map.webp'
 L.imageOverlay(image, bounds).addTo(map);
 map.fitBounds(bounds);
+
+var clickedCoordinates = [];
+
+
+var mode = 'pointer';
+var markers = [];
+
+// Event handler for marker mode button
+document.getElementById('markerMode').addEventListener('click', function() {
+  switchMode('marker');
+});
+
+// Event handler for path mode button
+document.getElementById('pathMode').addEventListener('click', function() {
+  switchMode('path');
+});
+
+// Click event listener on the map
+map.on('click', function(event) {
+  var clickedLatLng = event.latlng;
+
+  if (mode === 'marker') {
+    var marker = L.marker(clickedLatLng).addTo(map);
+    markers.push(marker);
+    mode = 'pointer';
+    switchMode(mode);
+  } else if (mode === 'path') {
+    clickedCoordinates.push(clickedLatLng);
+
+    if (clickedCoordinates.length > 1) {
+      var path = L.polyline(clickedCoordinates, { color: 'blue' }).addTo(map);
+    }
+
+  }
+});
+
+// Function to switch the mode
+function switchMode(newMode) {
+  mode = newMode;
+  document.getElementById('currentMode').textContent = 'Current mode: ' + mode.charAt(0).toUpperCase() + mode.slice(1);
+}
+
+
+
